b3a84b2d43e290856f28893ae0f4b4f8b638eaa8
kernmx <kernmx@mail.uc.edu>
2018-07-19 16:23:39 -0400

    Added the WebApp

---
+++ WebApp/static/scraper.js
@@ -0,0 +1,21 @@
+//Parameters
+var category = document.getElementById('category').value;
+var max_price = document.getElementById('max_price').value;
+var location = document.getElementById('location').value;
+
+var scraper = require('google-search-scraper');
+var DeathByCaptcha = require('deathbycaptcha');
+
+var dbc = new DeathByCaptcha('username', 'password');
+
+var options = {
+  query: category + "near" + location + "under" + max_price ,
+  age: 'y', // less than a year,
+  solver: dbc
+};
+
+scraper.search(options, function(err, url) {
+  // This is called for each result
+  if(err) throw err;
+  console.log(url)
+});
